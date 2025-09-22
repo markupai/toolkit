d6980c6a5e78be9e9d24482f6c9875525fc0126e
Thilo <125662076+thilo-m@users.noreply.github.com>
2025-09-22 15:14:15 +0200

    Cleanup sonar test coverage config

--- vitest.unit.config.ts
+++ vitest.unit.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'lcov'],
-      reportsDirectory: './coverage',
       exclude: ['node_modules/**', 'test/**', '**/*.d.ts', '**/*.test.ts', '**/*.config.ts', '**/types.ts'],
     },
   },
