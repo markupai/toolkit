40eb3142a7f1d8c293bbdea2594bde06006de1aa
Justin Horn <justin.horn@markup.ai>
2025-09-29 11:18:07 +0200

    fix: racing condition in test

---
+++ test/unit/testUtils.ts
@@ -0,0 +1,21 @@
+import { fail } from 'assert';
+import { ErrorType } from '../../src/utils/errors';
+import { expect, vi } from 'vitest';
+
+export const testTimeout = async (pollingFunction: () => Promise<any>, timeout: number) => {
+  let success = false;
+  try {
+    const promise = pollingFunction();
+    vi.advanceTimersByTime(timeout + 1);
+    await promise;
+    success = true;
+  } catch (error) {
+    expect(error).toBeDefined();
+    expect(error.type).toBe(ErrorType.TIMEOUT_ERROR);
+    expect(error.message).toContain('Workflow timed out');
+    expect(error.message).toContain('ms');
+  }
+  if (success) {
+    fail('Expected timeout error');
+  }
+};
